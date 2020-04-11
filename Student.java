b64b5caf1e4e11eb2a06e3f1b100aab3b71d94eb
armenski <62384817+armenski@users.noreply.github.com>
2020-04-11 23:41:02 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -7,14 +7,62 @@ class Student {
 	String lastName;
 	List<Integer> grades = new ArrayList<Integer>();
 	//TODO constructor
-
+	public Student(String index, String firstName, String lastName, List<Integer> grades) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades = grades;
+	}
 	//TODO seters & getters
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Integer> getGrades() {
+		return grades;
+	}
+
+	public void setGrades(List<Integer> grades) {
+		this.grades = grades;
+	}
 
 	public double getAverage() {
 		//TODO
+		int total = 0;
+		for(int gr : grades){
+			total = total + gr;
+		}
+		double averageGrade = (double) total / grades.size();
 	}
 
 	public int ECTSCredits() {
 		//TODO
+		int totalCredits = 0;
+		for(int tc : grades){
+			if(tc > 5 && tc <= 10){
+				totalCredits += 6;
+			}
+		}
+		return totalCredits;
 	}
 }
